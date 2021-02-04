9f587888f994bc895c5c2a5c1f360c10a6519446
YoungjaeLee <77099686+YoungJaeLee-github@users.noreply.github.com>
2021-02-05 00:49:15 +0900

    콘솔출력, 문자열 보간법(String Interpolation)

---
+++ Ex01/Ex01/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Ex01
+//
+//  Created by 이영재 on 2021/02/05.
+//
+//
+
+let someValue: String = "Hello World"
+print(someValue)
+dump(someValue)
+
+let someInt: Int = 26
+print("I'm \(someInt)")
