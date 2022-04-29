429edf4101dff0f096abebf6f36a85b8ff54c62b
YoungJae <dudwo8075@gmail.com>
2022-04-29 13:26:20 +0900

    Variables And Constants

---
+++ KxCoding/SourceCode/VariablesAndConstants/VariablesAndConstants/main.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  VariablesAndConstants
+//
+//  Created by 이영재 on 2022/04/29.
+//
+
+//MARK: - Variables : var variableName(: Type) = initialValue
+var name: String = "Swift"
+var year: Int = 2022
+var valid: Bool = false
+
+var x = 0.0, y = 0.0, z = 0.0
+
+print(name, year, valid, x, y, z, separator: "\n")
+
+name = "Swift2"
+name = "Swift3"
+
+print(name)
+
+//MARK: - Constants : let constantName(: Type) = initialValue
+let age: Int = 27
+// age = 26 Error!
+
+print(age)
